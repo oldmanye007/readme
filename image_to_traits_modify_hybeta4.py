Add the normalization to one standard solar zenith angle, and fix the problem of TOPO correction C factor estimation in flat area

image_to_traits_modify_hybeta4.py
@@ -144,10 +144,13 @@ def main():
     parser.add_argument("-coeffs", help="Trait coefficients directory", required=False, type = str)
     parser.add_argument("-nodata", help="New value to assign for no_data values", required=False, type = float, default =-9999)
     parser.add_argument("-smooth", help="BRDF smooth methods L: Linear regression; W: Weighted linear regression; I: Linear interpolation", required=False  , choices=['L', 'W', 'I'])
+    parser.add_argument("-sszn", help="standard solar zenith angle (degree)", required=False, type = float, default =40)
 
     args = parser.parse_args()
 
     traits = glob.glob("%s/*.json" % args.coeffs)
+    
+    std_solar_zn = float(args.sszn)/180*np.pi
     
     #Load data objects memory
     if args.img.endswith(".h5"):
@@ -339,10 +342,13 @@ def main():
         
         k_vol = generate_volume_kernel(hyObj.solar_az,hyObj.solar_zn,hyObj.sensor_az,hyObj.sensor_zn, ross = brdf_coeffs_List[first_effective_ibin]['ross'])
         k_geom = generate_geom_kernel(hyObj.solar_az,hyObj.solar_zn,hyObj.sensor_az,hyObj.sensor_zn,li = brdf_coeffs_List[first_effective_ibin]['li'])
-        k_vol_nadir = generate_volume_kernel(hyObj.solar_az,hyObj.solar_zn,hyObj.sensor_az,0, ross = brdf_coeffs_List[first_effective_ibin]['ross'])
-        k_geom_nadir = generate_geom_kernel(hyObj.solar_az,hyObj.solar_zn,hyObj.sensor_az,0,li = brdf_coeffs_List[first_effective_ibin]['li'])
         
-
+        if args.sszn is None:
+          k_vol_nadir = generate_volume_kernel(hyObj.solar_az,hyObj.solar_zn,hyObj.sensor_az,0, ross = brdf_coeffs_List[first_effective_ibin]['ross'])
+          k_geom_nadir = generate_geom_kernel(hyObj.solar_az,hyObj.solar_zn,hyObj.sensor_az,0,li = brdf_coeffs_List[first_effective_ibin]['li']) 
+        else:       
+          k_vol_nadir = generate_volume_kernel(np.pi,std_solar_zn,hyObj.sensor_az,0, ross = brdf_coeffs_List[first_effective_ibin]['ross'])
+          k_geom_nadir = generate_geom_kernel(np.pi,std_solar_zn,hyObj.sensor_az,0,li = brdf_coeffs_List[first_effective_ibin]['li'])
 
     if len(traits)!=0:
       

topo_brdf_coeffs_modify_hybeta4.py
@@ -23,6 +23,9 @@ NO_DATA_VALUE = -9999  #-0.9999  # -9999
 # The value that replace NaN in diagnostic tables
 DIAGNO_NAN_OUTPUT = -9999 
 
+# The C value for image with relatively flat terrain, it will make the TOPO correction factor to be close to 1
+FLAT_COEFF_C = -9999
+
 # Data range considered from image data in BRDF coefficient estimation
 REFL_MIN_THRESHOLD = 0.001 # 10
 REFL_MAX_THRESHOLD = 0.9  # 9000
@@ -44,6 +47,9 @@ SAMPLE_SLOPE_MIN_THRESHOLD = 0.03
 
 # BRDF coefficients in an NDVI bin with sample size less than this threshold will not be estimated in BRDF correction. Its coefficients might be estimated by its neighboring NDVI bins in later steps.
 MIN_SAMPLE_COUNT = 100
+
+# if there are too few pixels with terrain (above certian threshold), no TOPO correction factor will be close to 1
+MIN_SAMPLE_COUNT_TOPO = 100
 
 # Thresholds for BRDF correction. Pixels beyond this range will not be used for BRDF coefficients estimation.
 SENSOR_ZENITH_MIN_DEG = 2
@@ -496,8 +502,11 @@ def main():
                         sample_slope_sub = sample_slope[sample_index[i]:sample_index[i+1]]
                         sample_c1_sub = sample_c1[sample_index[i]:sample_index[i+1]]
 
-                        
-                        topo_coeff  = generate_topo_coeff_band(wave_samples_sub,(wave_samples_sub> REFL_MIN_THRESHOLD) & (wave_samples_sub< REFL_MAX_THRESHOLD) & (sample_cos_i_sub> COSINE_I_MIN_THRESHOLD) &  (sample_slope_sub> SLOPE_MIN_THRESHOLD) ,sample_cos_i_sub)
+                        if np.count_nonzero((sample_cos_i_sub> COSINE_I_MIN_THRESHOLD) &  (sample_slope_sub> SLOPE_MIN_THRESHOLD))>MIN_SAMPLE_COUNT_TOPO:
+                          topo_coeff  = generate_topo_coeff_band(wave_samples_sub,(wave_samples_sub> REFL_MIN_THRESHOLD) & (wave_samples_sub< REFL_MAX_THRESHOLD) & (sample_cos_i_sub> COSINE_I_MIN_THRESHOLD) &  (sample_slope_sub> SLOPE_MIN_THRESHOLD) ,sample_cos_i_sub)
+                        else:
+                          topo_coeff=FLAT_COEFF_C                       
+                          
                         topo_coeff_list[i]['c'].append(topo_coeff)
 
                         correctionFactor = (sample_c1_sub + topo_coeff)/(sample_cos_i_sub + topo_coeff)